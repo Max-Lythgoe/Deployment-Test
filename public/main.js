7180e5f6acf9e3e41386c7f2b39c8e15d1e0fd95
Max Lythgoe <lythgoemax@gmail.com>
2022-04-07 13:19:10 -0600

    added rollbar messages

--- public/main.js
+++ public/main.js
@@ -4,9 +4,11 @@ const Rollbar = require("rollbar");
 function darkMode() {
 	var element = document.body;
 	element.className = "dark-mode";
+    Rollbar.log('switched to darkmode')
 }
 
 function lightMode() {
 	var element = document.body;
 	element.className = "light-mode";
+    Rollbar.log('switched to lightmode')
 }
